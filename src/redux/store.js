fe754c181f5a3255960e223c46a8d8bf079a2e44
Sooraj Shukla <soorajshukla974@gmail.com>
2020-12-23 15:23:25 +0530

    add character reducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,9 @@ import {createStore, applyMiddleWare, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import {composeWithDevTools} from "redux-devtools-extension";
+import characterReducer from './characters/characterReducer';
 const rootReducer = combineReducers({
-
+    characters: characterReducer
 });
 const store = createStore(rootReducer,composeWithDevTools(applyMiddleWare(logger,thunk)));
 export default store;
