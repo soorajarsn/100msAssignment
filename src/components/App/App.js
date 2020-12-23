c18e736d1457f7aa0f0a12cd38c6aa96c9cc4643
Sooraj Shukla <soorajshukla974@gmail.com>
2020-12-23 14:47:21 +0530

    define theme for the App, add routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,18 @@
 import React from "react";
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.scss";
 import Index from "../Index/Index";
 
-function App(props){
+function App(props) {
+  return (
     <React.Fragment>
-        <Router>
-            <Switch>
-                <Route exact path="/" render = {props => <Index {...props}/>} />
-            </Switch>
-        </Router>
-        {/* Portals here */}
+      <Router>
+        <Switch>
+          <Route exact path="/" render={props => <Index {...props} />} />
+        </Switch>
+      </Router>
+      {/* Portals here */}
     </React.Fragment>
+  );
 }
 export default App;
