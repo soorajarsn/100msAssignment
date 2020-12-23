56fd302d0c7afe2a193ca0aa21ef0a1ef5626ce4
Sooraj Shukla <soorajshukla974@gmail.com>
2020-12-23 07:51:25 +0530

    add routes

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import "./App.scss";
+import Index from "../Index/Index";
+
+function App(props){
+    <React.Fragment>
+        <Router>
+            <Switch>
+                <Route exact path="/" render = {props => <Index {...props}/>} />
+            </Switch>
+        </Router>
+        {/* Portals here */}
+    </React.Fragment>
+}
+export default App;
