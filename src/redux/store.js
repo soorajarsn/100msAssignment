d12f8de660969c8ecc09b0338de8f72e57b01a1a
Sooraj Shukla <soorajshukla974@gmail.com>
2020-12-23 15:49:54 +0530

    remove bugs

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,10 @@
-import {createStore, applyMiddleWare, combineReducers} from 'redux';
-import thunk from 'redux-thunk';
+import {createStore, applyMiddleware, combineReducers} from 'redux';
 import logger from 'redux-logger';
 import {composeWithDevTools} from "redux-devtools-extension";
 import characterReducer from './characters/characterReducer';
+import thunk from 'redux-thunk';
 const rootReducer = combineReducers({
     characters: characterReducer
 });
-const store = createStore(rootReducer,composeWithDevTools(applyMiddleWare(logger,thunk)));
+const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(thunk,logger)));
 export default store;
