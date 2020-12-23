33a5f5a37b061bf07977fe6ffddd71da2dd27fe6
Sooraj Shukla <soorajshukla974@gmail.com>
2020-12-23 07:51:08 +0530

    touch homepage

---
+++ src/components/Index/Index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "Index.scss";
+function Index() {
+    return (
+        <React.Fragment>
+            <h1>Homepage</h1>
+        </React.Fragment>
+    )
+}
+
+export default Index
