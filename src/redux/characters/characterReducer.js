b07e2fef4a999e93d397de5886a23abcf7e29c65
Sooraj Shukla <soorajshukla974@gmail.com>
2020-12-23 15:22:32 +0530

    define reducer to handle character actions

---
+++ src/redux/characters/characterReducer.js
@@ -0,0 +1,18 @@
+import { GET_CHARACTER_REQUEST, GET_CHARACTER_SUCCESS, GET_CHARACTER_ERROR } from "./characterConsts"
+
+const initialState = {
+    characters = [],
+    loading:false,
+    error: ''
+}
+const characterReducer = (state = initialState, action) => {
+    switch(action.type){
+        case GET_CHARACTER_REQUEST:
+            return {...state, loading:true, error:''};
+        case GET_CHARACTER_SUCCESS:
+            return {laoding:false, characters: action.payload, error:''};
+        case GET_CHARACTER_ERROR:
+            return {...state, loading:false, error:action.payload};
+    }
+}
+export default characterReducer
