66a28fdd9aea518dddb789fb1a9f21efd5c21b29
Sooraj Shukla <soorajshukla974@gmail.com>
2020-12-24 13:50:17 +0530

    add route for character details

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.scss";
 import Index from "../Index/Index";
+import CharacterDetails from "../CharacterDetails/CharacterDetails";
 
 function App(props) {
   return (
@@ -9,6 +10,7 @@ function App(props) {
       <Router>
         <Switch>
           <Route exact path="/" render={props => <Index {...props} />} />
+          <Route exact path="/:id" render={props => <CharacterDetails {...props} />} />
         </Switch>
       </Router>
       {/* Portals here */}
