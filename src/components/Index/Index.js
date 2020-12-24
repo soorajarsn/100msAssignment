52ad415121396aded4729c0defbfd7c63f68a5fa
Sooraj Shukla <soorajshukla974@gmail.com>
2020-12-24 13:00:09 +0530

    add qs libray, filter by functionality, modify navbar styles

--- src/components/Index/Index.js
+++ src/components/Index/Index.js
@@ -3,11 +3,12 @@ import "./Index.scss";
 import Navbar from "../Navbar/Navbar";
 import { getCharacters } from "../../redux";
 import { connect } from "react-redux";
+import qs from "qs";
 function FilterByCategory(props) {
-  const [category, setCategory] = useState("Category");
+  const [category, setCategory] = useState(qs.parse(props.location.search,{ignoreQueryPrefix:true}).category || "Category");
   const handleCatgoryChange = event => {
-    //handle change;
     setCategory(event.target.value);
+    props.history.push('/?category='+event.target.value+'&page=1');
   };
   return (
     <div className="filterby-section center">
@@ -23,7 +24,6 @@ function FilterByCategory(props) {
   );
 }
 function Character(props) {
-    console.log(props);
   return (
     <div className="character-card full-width">
       <ul className="character-info">
@@ -59,29 +59,88 @@ function Character(props) {
   );
 }
 function CategoryFilter_and_Characters(props) {
+  const pageNumber = parseInt(props.page);
+  const queryStringParams = props.location.search.split("&");
+  const gotoPrevPage = () => {
+    let queryString = "";
+    for (var i = 0; i < queryStringParams.length; i++) {
+      if (queryStringParams[i].includes("page")) {
+        if (i == 0) queryString += "?page=" + (pageNumber - 1);
+        else queryString += "&page=" + (pageNumber - 1);
+      } else {
+        queryString += queryStringParams[i];
+      }
+    }
+    props.history.push('/'+queryString);
+  };
+  const gotoNextPage = () => {
+    let queryString = "";
+    for (var i = 0; i < queryStringParams.length; i++) {
+      if (queryStringParams[i].includes("page")) {
+        if (i == 0) queryString += "?page=" + (pageNumber + 1);
+        else queryString += "&page=" + (pageNumber + 1);
+      } else {
+        queryString += queryStringParams[i];
+      }
+    }
+    props.history.push("/"+queryString);
+  };
   return (
     <div className="character-parent-container medium-padding-left medium-padding-right center">
       <div className="container flex full-width justify-space-between">
         <div className="filter-container">
-          <FilterByCategory />
+          <FilterByCategory {...props} />
         </div>
-        <div className="page-content full-width">
+        <div className="page-content full-width flex flex-column align-center">
           <div className="characters-container full-width">
-              {props.characters.map(character => <Character key={character.char_id} {...character} />)}
+            {props.characters.map(character => (
+              <Character key={character.char_id} {...character} />
+            ))}
+          </div>
+          <div className="pagination flex align-center">
+            <button className="prev-page small-margin-right color-white center" onClick={gotoPrevPage} disabled={pageNumber == 1}>
+              <i className="fas fa-angle-left" />
+            </button>
+            <div className="current-page">{props.page}</div>
+            <button className="next-page small-margin-left color-white center" onClick={gotoNextPage} disabled={props.characters.length < 10}>
+              <i className="fas fa-angle-right" />
+            </button>
           </div>
         </div>
       </div>
     </div>
   );
 }
+const constructQueryString = queryObject => {
+  let offset = 0;
+  let queryString = "?limit=10";
+  for (let key in queryObject) {
+    if (key == "page") {
+      offset = (parseInt(queryObject.page) - 1) * 10;
+      queryString += `&offset=${offset}`;
+    } else {
+      queryString += `&${key}=${queryObject[key]}`;
+    }
+  }
+  return queryString;
+};
 function Index(props) {
+  const queryObject = qs.parse(props.location.search, { ignoreQueryPrefix: true });
+  const page = queryObject.page;
+  const name = queryObject.name;
+  const category = queryObject.category;
+  //scroll to top, get the characters;
   useEffect(() => {
-    props.getCharacters("characters?limit=10&offset=10");
-  }, []);
+    window.scrollTo(0, 0); //to scroll at the top
+    const queryString = constructQueryString(queryObject);
+    console.log('getting from endpoint '+queryString);
+    props.getCharacters("characters" + queryString);
+  }, [page, name, category]); //run this effect whenever any of page, name or category changes
+
   return (
     <React.Fragment>
-      <Navbar />
-      <CategoryFilter_and_Characters characters={props.characters} />
+      <Navbar {...props} />
+      <CategoryFilter_and_Characters {...props} page={page || 1} />
     </React.Fragment>
   );
 }
