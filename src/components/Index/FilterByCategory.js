6e08a2451758dba52c2e0202e06fdff5881efbd5
Sooraj Shukla <soorajshukla974@gmail.com>
2020-12-24 13:37:28 +0530

    remove bug: apply filters correctly

--- src/components/Index/FilterByCategory.js
+++ src/components/Index/FilterByCategory.js
@@ -1,17 +1,21 @@
 import React, {useState} from "react";
 import qs from "qs";
 function FilterByCategory(props) {
-  const [category, setCategory] = useState(qs.parse(props.location.search, { ignoreQueryPrefix: true }).category || "Category");
+    const currCategory = qs.parse(props.location.search, { ignoreQueryPrefix: true }).category;
+  const [category, setCategory] = useState((currCategory && currCategory.split(' ').join("+")) || "");
   const handleCatgoryChange = event => {
     setCategory(event.target.value);
-    props.history.push("/?category=" + event.target.value + "&page=1");
+    if(event.target.value)
+        props.history.push("/?category=" + event.target.value + "&page=1");
+    else 
+        props.history.push("/?page=1");
   };
   return (
     <div className="filterby-section center">
       <div className="container flex align-center justify-space-between full-width">
         <h2 className="light-bold-font small-size-font small-margin-left">Filter By</h2>
         <select value={category} onChange={handleCatgoryChange} className="small-margin-right">
-          <option value="Category"> Category</option>
+          <option value=""> Category</option>
           <option value="Breaking+Bad">Breaking Bad</option>
           <option value="Better+Call+Saul">Better Call Saul</option>
         </select>
